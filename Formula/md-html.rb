2de882c0e3f773046f8a8f17d01f6ed6f82a5f2a
GINTOAHC <chaotingchen10@gmail.com>
2024-10-09 16:20:20 +0800

    feat: Automation script for releasing

--- Formula/md-html.rb
+++ Formula/md-html.rb
@@ -9,15 +9,15 @@ class MdHtml < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/GNITOAHC/md-html/releases/download/v0.1.0/md-html-v#{version}-darwin-arm64.tar.gz"
-      sha256 "f5c7e08a0dd422920e7146044f06506dde9dcb39248806180f6ab9a2a89e62ac" # darwin_arm64
+      sha256 "f5c7e08a0dd422920e7146044f06506dde9dcb39248806180f6ab9a2a89e62ac" # darwin-arm64
     else
       url "https://github.com/GNITOAHC/md-html/releases/download/v0.1.0/md-html-v#{version}-darwin-amd64.tar.gz"
-      sha256 "9b133f518c140737d3b783eb00a5dcd443c89b37b1f0dd38eedc6feef4b9bf72" # darwin_amd65
+      sha256 "9b133f518c140737d3b783eb00a5dcd443c89b37b1f0dd38eedc6feef4b9bf72" # darwin-amd64
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/GNITOAHC/md-html/releases/download/v0.1.0/md-html-v#{version}-linux-amd64.tar.gz"
-      sha256 "284439e11e6033fde0ceddf0c582caeae4dc41df1a78f1844087968d6e02186e" # linux_amd64
+      sha256 "284439e11e6033fde0ceddf0c582caeae4dc41df1a78f1844087968d6e02186e" # linux-amd64
     end
   else
     odie "Unsupported platform."
