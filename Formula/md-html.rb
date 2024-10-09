d4c717225e093ecb1688324dcd83936b9276f23d
GINTOAHC <chaotingchen10@gmail.com>
2024-10-09 16:27:18 +0800

    Release md-html v0.2.0

--- Formula/md-html.rb
+++ Formula/md-html.rb
@@ -2,22 +2,22 @@ class MdHtml < Formula
   desc "Lightweight markdown to html converter"
   homepage "https://github.com/gnitoahc/md-html"
   license "MIT"
-  version "0.1.0"
+  version "0.2.0"
 
   # depends_on "go" => :build
   
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/GNITOAHC/md-html/releases/download/v0.1.0/md-html-v#{version}-darwin-arm64.tar.gz"
-      sha256 "f5c7e08a0dd422920e7146044f06506dde9dcb39248806180f6ab9a2a89e62ac" # darwin-arm64
+      sha256 "5695911532eb2a3ab64991b0711651613a407de7f0039d591b1f2546d5c72b4a" # darwin-arm64
     else
       url "https://github.com/GNITOAHC/md-html/releases/download/v0.1.0/md-html-v#{version}-darwin-amd64.tar.gz"
-      sha256 "9b133f518c140737d3b783eb00a5dcd443c89b37b1f0dd38eedc6feef4b9bf72" # darwin-amd64
+      sha256 "cb37b264c7c8b0c5123ae9a87aca77e7891d6d4194447586594818f24dce1e80" # darwin-amd64
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/GNITOAHC/md-html/releases/download/v0.1.0/md-html-v#{version}-linux-amd64.tar.gz"
-      sha256 "284439e11e6033fde0ceddf0c582caeae4dc41df1a78f1844087968d6e02186e" # linux-amd64
+      sha256 "c282e224073150c81e6b5389d04e4163eccd49f3670b34c395097504d6e840e5" # linux-amd64
     end
   else
     odie "Unsupported platform."
