d75ec9575cb42b266e651beb65acb075aa73ba94
GINTOAHC <chaotingchen10@gmail.com>
2025-07-19 21:35:46 +0800

    Release queue v0.1.1

--- Formula/queue.rb
+++ Formula/queue.rb
@@ -2,17 +2,17 @@ class Queue < Formula
   desc "Lightweight binary to submit queue jobs to local machine's background"
   homepage "https://github.com/gnitoahc/processq"
   license "MIT"
-  version "0.1.0"
+  version "0.1.1"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/GNITOAHC/processq/releases/download/v#{version}/queue-v#{version}-darwin-arm64.tar.gz"
-      sha256 "4e104a94eaee28caa1506c75462c67460e8488fe2256a70b9ee9086ac55b3bef" # darwin-arm64
+      sha256 "e776b19c1ac0ea6875ea5eaf52ef34208e79e9c1ffb7e7d0516c57373d309b55" # darwin-arm64
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/GNITOAHC/md-html/releases/download/v#{version}/queue-v#{version}-linux-amd64.tar.gz"
-      sha256 "6cde74ce466ae5bad5ae06047b51d624ca1901418e782e7f512b8fdf7d2b586f" # linux-amd64
+      sha256 "b585998e616383ff3fef1bb28b6111593594aedd08626abaa52157c629502566" # linux-amd64
     end
   else
     odie "Unsupported platform."
